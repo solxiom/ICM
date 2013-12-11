52244aa7e8cacdc16b79c84d489cf276732bc5e2
Kavan Soleimanbeigi <solxiom@gmail.com>
2013-12-11 23:42:12 +0200

    JSON support installed via project pom

--- icmserver/src/main/java/com/coderleopard/icmserver/controller/HomeController.java
+++ icmserver/src/main/java/com/coderleopard/icmserver/controller/HomeController.java
@@ -1,11 +1,19 @@
 package com.coderleopard.icmserver.controller;
 
+import com.coderleopard.icmserver.config.SpringMongoConfig;
+import com.coderleopard.icmserver.domain.TestUser;
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,5 +22,16 @@ public class HomeController {
 	@RequestMapping(value="/")
 	public ModelAndView test(HttpServletResponse response) throws IOException{
 		return new ModelAndView("home");
+	}
+        
+	@RequestMapping(value="/user")
+	public @ResponseBody TestUser test2(HttpServletResponse response) throws IOException{
+            ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringMongoConfig.class);
+            MongoOperations mongoOperation = (MongoOperations) ctx.getBean("mongoTemplate");
+            TestUser user = new TestUser("Kavan", "password123");
+            mongoOperation.save(user);
+            
+            user = mongoOperation.findOne(new Query(Criteria.where("username").is("Kavan")), TestUser.class);
+            return user;
 	}
 }
